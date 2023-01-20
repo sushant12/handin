4ff207ccefabbb9a7c71b1cd274d0507deb02469
sushant12 <sussyoung9@gmail.com>
2023-01-20 18:05:16 +0545

    integrated tailwind css

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :handin, HandinWeb.Endpoint,
   secret_key_base: "Ca2n5MXS6IFLXLHJM0ZZ61yWOt6YY/Ch+xdy3KAyxpZalhxsduMkwOjCjxND/pnb",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
