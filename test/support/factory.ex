0a733e56262deadd6482f62115d47292067fe8fc
sushant12 <sussyoung9@gmail.com>
2023-01-23 18:17:52 +0545

    updated factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,25 +5,22 @@ defmodule HandinWeb.Factory do
   alias Handin.Modules.Module
 
   defp valid_user_password, do: "hello world!"
+  @now NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
 
   def admin_factory do
-    now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
-
     %User{
       email: "admin@admin.com",
       hashed_password: Bcrypt.hash_pwd_salt("admin"),
-      confirmed_at: now,
+      confirmed_at: @now,
       role: "admin"
     }
   end
 
   def course_admin_factory do
-    now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
-
     %User{
-      email: sequence(:email, &"#{&1}@studentmail.ul.ie"),
+      email: sequence(:email, &"course admin#{&1}@studentmail.ul.ie"),
       hashed_password: Bcrypt.hash_pwd_salt(valid_user_password()),
-      confirmed_at: now,
+      confirmed_at: @now,
       role: "course_admin"
     }
   end
@@ -38,6 +35,15 @@ defmodule HandinWeb.Factory do
   def module_factory do
     %Module{
       name: sequence(:name, &"M#{&1}")
+    }
+  end
+
+  def teacher_factory do
+    %User{
+      email: sequence(:email, &"teacher#{&1}@studentmail.ul.ie"),
+      hashed_password: Bcrypt.hash_pwd_salt(valid_user_password()),
+      confirmed_at: @now,
+      role: "teacher"
     }
   end
 end
