ff669e0a7b805c50ba9be2358fc8f4fd90fa5653
sushant12 <sussyoung9@gmail.com>
2023-07-25 20:03:25 +0545

    modals fixed

--- lib/handin_web/live/admin/university_live/form_component.ex
+++ lib/handin_web/live/admin/university_live/form_component.ex
@@ -7,10 +7,12 @@ defmodule HandinWeb.Admin.UniversityLive.FormComponent do
   def render(assigns) do
     ~H"""
     <div>
-      <.header>
-        <%= @title %>
-        <:subtitle>Use this form to manage university records in your database.</:subtitle>
-      </.header>
+      <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+        <.header class="text-lg font-semibold text-gray-900 dark:text-white">
+          <%= @title %>
+          <:subtitle>Use this form to manage university records in your database.</:subtitle>
+        </.header>
+      </div>
 
       <.simple_form
         for={@form}
@@ -19,10 +21,47 @@ defmodule HandinWeb.Admin.UniversityLive.FormComponent do
         phx-change="validate"
         phx-submit="save"
       >
-        <.input field={@form[:name]} type="text" label="Name" />
-        <.input field={@form[:config]} type="text" label="Config" />
+        <div class="grid gap-4 mb-4 sm:grid-cols-1">
+          <div>
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Name
+            </label>
+            <.input
+              field={@form[:name]}
+              type="text"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            />
+          </div>
+          <div>
+            <label for="config" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Config
+            </label>
+            <.input
+              field={@form[:config]}
+              type="text"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            />
+          </div>
+        </div>
         <:actions>
-          <.button phx-disable-with="Saving...">Save University</.button>
+          <.button
+            phx-disable-with="Saving..."
+            class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            <svg
+              class="mr-1 -ml-1 w-6 h-6"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                clip-rule="evenodd"
+              >
+              </path>
+            </svg>Save University
+          </.button>
         </:actions>
       </.simple_form>
     </div>
