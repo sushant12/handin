7eb46977d675d701cffa9e1927bf37bdd07c7e0e
padmaJS <padshak230@gmail.com>
2023-08-14 17:30:09 +0545

    add files via test edit

--- lib/handin_web/live/assignment_live/assignment_test_component.ex
+++ lib/handin_web/live/assignment_live/assignment_test_component.ex
@@ -30,7 +30,26 @@ defmodule HandinWeb.AssignmentLive.AssignmentTestComponent do
         <%= for entry <- @uploads.test_support_file.entries do %>
           <article class="upload-entry">
             <figure class="flex">
-            <svg width="1.25rem" height="1.25rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9 17H15M9 13H15M9 9H10M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V9M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19" stroke="#707070" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+              <svg
+                width="1.25rem"
+                height="1.25rem"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M9 17H15M9 13H15M9 9H10M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V9M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19"
+                    stroke="#707070"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                  </path>
+                </g>
+              </svg>
               <figcaption><%= entry.client_name %></figcaption>
             </figure>
           </article>
@@ -93,6 +112,7 @@ defmodule HandinWeb.AssignmentLive.AssignmentTestComponent do
            assignment_test_params
          ) do
       {:ok, assignment_test} ->
+        consume_entries(socket, assignment_test)
         notify_parent({:saved, assignment_test})
 
         {:noreply,
@@ -108,16 +128,7 @@ defmodule HandinWeb.AssignmentLive.AssignmentTestComponent do
   defp save_assignment_test(socket, :add_assignment_test, assignment_test_params) do
     case AssignmentTests.create_assignment_test(assignment_test_params) do
       {:ok, assignment_test} ->
-        consume_uploaded_entries(socket, :test_support_file, fn meta, entry ->
-          AssignmentTests.upload_test_support_file(%{
-            "file" => %Plug.Upload{
-              content_type: entry.client_type,
-              filename: entry.client_name,
-              path: meta.path
-            },
-            "assignment_test_id" => assignment_test.id
-          })
-        end)
+        consume_entries(socket, assignment_test)
 
         notify_parent({:saved, assignment_test})
 
@@ -129,6 +140,19 @@ defmodule HandinWeb.AssignmentLive.AssignmentTestComponent do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign_form(socket, changeset)}
     end
+  end
+
+  defp consume_entries(socket, assignment_test) do
+    consume_uploaded_entries(socket, :test_support_file, fn meta, entry ->
+      AssignmentTests.upload_test_support_file(%{
+        "file" => %Plug.Upload{
+          content_type: entry.client_type,
+          filename: entry.client_name,
+          path: meta.path
+        },
+        "assignment_test_id" => assignment_test.id
+      })
+    end)
   end
 
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
