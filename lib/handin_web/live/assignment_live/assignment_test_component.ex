925d1c96a4fe7f61e5f2d26cf43fd87b87a58541
padmaJS <padshak230@gmail.com>
2023-08-21 23:15:19 +0545

    icons for add and delete

--- lib/handin_web/live/assignment_live/assignment_test_component.ex
+++ lib/handin_web/live/assignment_live/assignment_test_component.ex
@@ -26,11 +26,68 @@ defmodule HandinWeb.AssignmentLive.AssignmentTestComponent do
 
         <div>
           <label>Commands</label>
-          <.button type="button" phx-click="add_command_fields" phx-target={@myself}>Add</.button>
+          <.button type="button" phx-click="add_command_fields" phx-target={@myself}>
+            <svg
+              width="1.25rem"
+              height="1.25rem"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="#2ac666"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  d="M11 8C11 7.44772 11.4477 7 12 7C12.5523 7 13 7.44772 13 8V11H16C16.5523 11 17 11.4477 17 12C17 12.5523 16.5523 13 16 13H13V16C13 16.5523 12.5523 17 12 17C11.4477 17 11 16.5523 11 16V13H8C7.44771 13 7 12.5523 7 12C7 11.4477 7.44772 11 8 11H11V8Z"
+                  fill="#2ac666"
+                >
+                </path>
+
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12ZM3.00683 12C3.00683 16.9668 7.03321 20.9932 12 20.9932C16.9668 20.9932 20.9932 16.9668 20.9932 12C20.9932 7.03321 16.9668 3.00683 12 3.00683C7.03321 3.00683 3.00683 7.03321 3.00683 12Z"
+                  fill="#2ac666"
+                >
+                </path>
+              </g>
+            </svg>
+          </.button>
           <.inputs_for :let={f} field={@form[:commands]}>
-            <div class="grid grid-cols-2 gap-4 mb-2">
-              <.input field={f[:name]} label="Name" type="text" />
-              <.input field={f[:command]} label="Command" type="text" />
+            <div class="grid grid-cols-7 gap-4 mb-2">
+              <div class="col-span-3">
+                <.input field={f[:name]} label="Name" type="text" />
+              </div>
+              <div class="col-span-3">
+                <.input field={f[:command]} label="Command" type="text" />
+              </div>
+              <.button
+                type="button"
+                phx-click="remove_command_fields"
+                phx-value-index={f.index}
+                phx-target={@myself}
+                class="flex justify-center pt-9"
+              >
+                <svg
+                  fill="#ee3f3f"
+                  width="1.15rem"
+                  height="1.15rem"
+                  viewBox="0 0 16 16"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="#ee3f3f"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      d="M0 14.545L1.455 16 8 9.455 14.545 16 16 14.545 9.455 8 16 1.455 14.545 0 8 6.545 1.455 0 0 1.455 6.545 8z"
+                      fill-rule="evenodd"
+                    >
+                    </path>
+                  </g>
+                </svg>
+              </.button>
             </div>
             <.input
               field={f[:fail]}
@@ -39,14 +96,6 @@ defmodule HandinWeb.AssignmentLive.AssignmentTestComponent do
               phx-click="toggle_expected_output"
             />
             <.input field={f[:expected_output]} label="Expected output" type="text" />
-            <.button
-              type="button"
-              phx-click="remove_command_fields"
-              phx-value-index={f.index}
-              phx-target={@myself}
-            >
-              Delete command
-            </.button>
           </.inputs_for>
         </div>
 
