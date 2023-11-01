033764d98f1c59045669f7f38e58566efd0c06ee
sushant12 <sussyoung9@gmail.com>
2023-11-01 14:06:33 +0545

    [94] preview test_support_files

--- lib/handin_web/live/assignment_live/assignment_test_component.ex
+++ lib/handin_web/live/assignment_live/assignment_test_component.ex
@@ -123,6 +123,33 @@ defmodule HandinWeb.AssignmentLive.AssignmentTestComponent do
             upload={@uploads.test_support_file}
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
           />
+          <%= for test_support_file <- @assignment_test.test_support_files do %>
+            <article :if={test_support_file.file} class="upload-entry">
+              <figure class="flex">
+                <svg
+                  width="1.25rem"
+                  height="1.25rem"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_iconCarrier">
+                    <path
+                      d="M9 17H15M9 13H15M9 9H10M13 3H8.2C7.0799 3 6.51984 3 6.09202 3.21799C5.71569 3.40973 5.40973 3.71569 5.21799 4.09202C5 4.51984 5 5.0799 5 6.2V17.8C5 18.9201 5 19.4802 5.21799 19.908C5.40973 20.2843 5.71569 20.5903 6.09202 20.782C6.51984 21 7.0799 21 8.2 21H15.8C16.9201 21 17.4802 21 17.908 20.782C18.2843 20.5903 18.5903 20.2843 18.782 19.908C19 19.4802 19 18.9201 19 17.8V9M13 3L19 9M13 3V7.4C13 7.96005 13 8.24008 13.109 8.45399C13.2049 8.64215 13.3578 8.79513 13.546 8.89101C13.7599 9 14.0399 9 14.6 9H19"
+                      stroke="#707070"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                    </path>
+                  </g>
+                </svg>
+                <figcaption><%= test_support_file.file.file_name %></figcaption>
+              </figure>
+            </article>
+          <% end %>
           <%= for entry <- @uploads.test_support_file.entries do %>
             <article class="upload-entry">
               <figure class="flex">
