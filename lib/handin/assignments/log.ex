a7b2bb0edd41791e1de31d4a1dd1ccfe4a3b9eb8
sushant12 <sussyoung9@gmail.com>
2023-11-20 15:33:00 +0545

    create index in logs

--- lib/handin/assignments/log.ex
+++ lib/handin/assignments/log.ex
@@ -2,12 +2,14 @@ defmodule Handin.Assignments.Log do
   use Handin.Schema
 
   import Ecto.Changeset
-  alias Handin.Assignments.Build
+  alias Handin.Assignments.{Build, Command}
   @type t :: %__MODULE__{}
   schema "logs" do
     field :description, :string
 
     belongs_to :build, Build
+
+    belongs_to :command, Command
 
     timestamps(type: :utc_datetime_usec)
   end
