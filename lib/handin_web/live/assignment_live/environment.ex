7de55e94800b2ddecf2d7aa129a90b44639cddd4
sushant12 <sussyoung9@gmail.com>
2023-11-26 14:58:09 +0000

    remove some template

--- lib/handin_web/live/assignment_live/environment.ex
+++ lib/handin_web/live/assignment_live/environment.ex
@@ -4,6 +4,63 @@ defmodule HandinWeb.AssignmentLive.Environment do
   alias Handin.Modules
   alias Handin.Assignments
   alias Handin.ProgrammingLanguages
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <.breadcrumbs>
+      <:item text="Home" href={~p"/"} />
+      <:item text="Modules" href={~p"/modules"} />
+      <:item text={@module.name} href={~p"/modules/#{@module.id}/assignments"} />
+      <:item
+        text="Assignments"
+        href={~p"/modules/#{@module.id}/assignments/#{@assignment.id}"}
+        current={true}
+      />
+    </.breadcrumbs>
+
+    <.tabs>
+      <:item text="Details" href={~p"/modules/#{@module.id}/assignments/#{@assignment.id}/details"} />
+      <:item
+        text="Environment"
+        href={~p"/modules/#{@module.id}/assignments/#{@assignment.id}/environment"}
+        current={true}
+      />
+      <:item text="Tests" href={~p"/modules/#{@module.id}/assignments/#{@assignment.id}/tests"} />
+      <:item
+        text="Submissions"
+        href={~p"/modules/#{@module.id}/assignments/#{@assignment.id}/submissions"}
+      />
+    </.tabs>
+
+    <div>
+      <.simple_form for={@form} id="environment-setup-form">
+        <.input
+          field={@form[:programming_language_id]}
+          label="Language"
+          type="select"
+          prompt="Select Programming Language"
+          options={@programming_languages}
+        />
+
+        <LiveMonacoEditor.code_editor
+          style="min-height: 450px; width: 100%;"
+          opts={
+            Map.merge(
+              LiveMonacoEditor.default_opts(),
+              %{"language" => "shell"}
+            )
+          }
+        />
+      </.simple_form>
+      <div id="helper-files-container">
+        <%!-- use table component here to display helper files --%>
+      </div>
+      <div id="support-files-container">
+        <%!-- use table component here to display helper files --%>
+      </div>
+    </div>
+    """
+  end
 
   @impl true
   def mount(%{"id" => id, "assignment_id" => assignment_id}, _session, socket) do
