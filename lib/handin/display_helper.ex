afb8531db56ff920007fdd4a40d23616cfac69a3
Rijan-Budds <buddsrijan@gmail.com>
2023-12-25 16:31:26 +0545

    helper function

--- lib/handin/display_helper.ex
+++ lib/handin/display_helper.ex
@@ -1,6 +1,8 @@
 defmodule Handin.DisplayHelper do
   use Timex
 
+  def format_date(nil, _timezone), do: "N/A"
+
   def format_date(date, timezone) do
     converted_date = Timex.Timezone.convert(date, timezone)
     Timex.format!(converted_date, "%b %e, %Y at %H:%M:%S %p", :strftime)
