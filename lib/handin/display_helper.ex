39775244d01bc11b5a93d974757f40621911c624
Rijan-Budds <buddsrijan@gmail.com>
2023-12-25 16:26:15 +0545

    format_date/1 for nil case

---
+++ lib/handin/display_helper.ex
@@ -0,0 +1,10 @@
+defmodule Handin.DisplayHelper do
+  use Timex
+
+  def format_date(nil, _timezone), do: "N/A"
+
+  def format_date(date, timezone) do
+    converted_date = Timex.Timezone.convert(date, timezone)
+    Timex.format!(converted_date, "%b %e, %Y at %H:%M:%S %p", :strftime)
+  end
+end
