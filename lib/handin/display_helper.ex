b12250b3e1fcf9cd3ffe0c36664976cdef3b1ac3
sushant12 <sussyoung9@gmail.com>
2023-12-26 20:43:56 +0000

    save future datetime as naive

--- lib/handin/display_helper.ex
+++ lib/handin/display_helper.ex
@@ -1,8 +1,8 @@
 defmodule Handin.DisplayHelper do
   use Timex
 
-  def format_date(date, timezone) do
-    converted_date = Timex.Timezone.convert(date, timezone)
-    Timex.format!(converted_date, "%b %e, %Y at %H:%M:%S %p", :strftime)
+  def format_date(date_time, _timezone) do
+    # converted_date = Timex.Timezone.convert(date, timezone)
+    Timex.format!(date_time, "%b %e, %Y at %H:%M:%S %p", :strftime)
   end
 end
