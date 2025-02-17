6d092ea7773eb7d94f6d905c7e24367129a95002
sushant12 <sussyoung9@gmail.com>
2025-02-17 17:18:45 +0000

    show the always_pass_test option in the test

--- lib/handin_web/live/assignment_live/tests.ex
+++ lib/handin_web/live/assignment_live/tests.ex
@@ -203,7 +203,7 @@ defmodule HandinWeb.AssignmentLive.Tests do
               label="Test Sleep Duration (minutes)"
               step="1"
             />
-
+            <.input field={@form[:always_pass_test]} type="checkbox" label="Always Pass Test" />
             <.button
               class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               phx-disable-with="Saving..."
